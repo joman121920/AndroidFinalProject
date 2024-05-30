8b6be51cdbdc8930e2cbc8960076039fa546b3cf
Lance Antor <lanceantor335@gmail.com>
2024-05-30 08:28:22 +0800

    ye

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,10 +34,10 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.gms:play-services-auth:21.1.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.12.0")
